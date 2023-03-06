bb778bdb14158872ab3ee9cf18c1568566bc1a6c
masudrahman-dev <masudrahman.dev@gmail>
2023-03-06 06:12:37 +0600

    show modal data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const aiTools = 'https://openapi.programming-hero.com/api/ai/tools';
-
+const modalData = 'https://openapi.programming-hero.com/api/ai/tool';
 const allData = () => {
   fetch(aiTools)
     .then((res) => res.json())
@@ -45,7 +45,7 @@ const displayData = (data) => {
                 <p>${published_in}</p>
             </div>
 
-            <span onclick="modal_data('${id}')" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <span onclick="getModalData('${id}')" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <img class="shadow-sm rounded-pill" src="./images/datails-icon.svg" alt="">
             </span>
         </div>
@@ -76,153 +76,151 @@ const featuresList = (data) => {
 
 allData();
 
-// const modal_data = (id) => {
-//   const modal_data = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
-//   console.log(modal_data);
-//   fetch(modal_data)
-//     .then((res) => res.json())
-//     .then((modal_data) => {
-//       show_modal(modal_data.data);
-//     });
-// };
+const getModalData = (id) => {
+  fetch(modalData + '/' + id)
+    .then((res) => res.json())
+    .then((modal_data) => {
+      // show_modal(modal_data.data);
+      showModalData(modal_data.data);
+    });
+};
 
-// const show_modal = (modal_data) => {
-//   // console.log(modal_data);
-//   const { accuracy, description, image_link, features, input_output_examples } =
-//     modal_data;
+const showModalData = (modal_data) => {
+  console.log(modal_data);
+  // console.log(modal_data);
+  const { accuracy, description, image_link, features, input_output_examples } =
+    modal_data;
 
-//   // console.log(accuracy.score === null);
+  // console.log(accuracy.score === null);
 
-//   const features_data = Object.values(features);
+  const features_data = Object.values(features);
 
-//   const modal_body = (document.getElementById('modal_body').innerHTML = `
+  const modal_body = (document.getElementById('modal_body').innerHTML = `
 
-//   <button type="button"
-//   class="btn-close rounded-pill bg-warning p-3 position-absolute top-0 start-100 translate-middle"
-//   data-bs-dismiss="modal" aria-label="Close">
-//   </button>
+    <button type="button"
+    class="btn-close rounded-pill bg-warning p-3 position-absolute top-0 start-100 translate-middle"
+    data-bs-dismiss="modal" aria-label="Close">
+    </button>
 
-//  <div class="d-flex gap-3 flex-column flex-xl-row">
+   <div class="d-flex gap-3 flex-column flex-xl-row">
 
-//   <div class="order-1 order-xl-0">
-//       <h4 class="display-6 fw-bold text-light text-center text-xl-start ">${description}</h4>
+    <div class="order-1 order-xl-0">
+        <h4 class="display-6 fw-bold text-light text-center text-xl-start ">${description}</h4>
 
-//       <div id="price_box" class="d-flex gap-3 text-center my-5">
+        <div id="price_box" class="d-flex gap-3 text-center my-5">
 
-//       </div>
-//       <div class="">
-//           <div class="row mt-5 text-light">
-//               <div class="col">
-//                   <h5>Features</h5>
-//                   <ul id="featuresListModal">
+        </div>
+        <div class="">
+            <div class="row mt-5 text-light">
+                <div class="col">
+                    <h5>Features</h5>
+                    <ul id="featuresListModal">
 
-//                   </ul>
-//               </div>
-//               <div class="col">
-//                   <h5>Integrations</h5>
-//                   <ul id="modal_Integrations">
+                    </ul>
+                </div>
+                <div class="col">
+                    <h5>Integrations</h5>
+                    <ul id="modal_Integrations">
 
-//                   </ul>
-//               </div>
-//           </div>
+                    </ul>
+                </div>
+            </div>
 
-//       </div>
+        </div>
 
-//   </div>
+    </div>
 
-//   <div class="order-0 order-xl-1">
-//       <div class="position-relative">
-//           <button id="modal_accuracy" class="btn btn-primary mt-3 me-3 position-absolute top-0 end-0">${
-//             accuracy.score === null
-//               ? 'not found'
-//               : accuracy.score + '%' + ' accuracy'
-//           }
-//              </button>
-//           <img class="img-fluid rounded" src="${image_link[0]}" alt="">
-//       </div>
-//       <div class="text-light text-center mt-3">
-//           <h4>${
-//             input_output_examples === null
-//               ? 'not found'
-//               : input_output_examples[0].input
-//           }</h4>
-//           <p>${
-//             input_output_examples === null
-//               ? 'not found'
-//               : input_output_examples[0].output
-//           }</p>
-//       </div>
+    <div class="order-0 order-xl-1">
+        <div class="position-relative">
+            <button id="modal_accuracy" class="btn btn-primary mt-3 me-3 position-absolute top-0 end-0">${
+              accuracy.score === null
+                ? 'not found'
+                : accuracy.score + '%' + ' accuracy'
+            }
+               </button>
+            <img class="img-fluid rounded" src="${image_link[0]}" alt="">
+        </div>
+        <div class="text-light text-center mt-3">
+            <h4>${
+              input_output_examples === null
+                ? 'not found'
+                : input_output_examples[0].input
+            }</h4>
+            <p>${
+              input_output_examples === null
+                ? 'not found'
+                : input_output_examples[0].output
+            }</p>
+        </div>
 
-//   </div>
-// </div>
+    </div>
+  </div>
 
-//   `);
-//   // Modal features
-//   const featuresListModal = document.getElementById('featuresListModal');
-//   // console.log(modal_data.features);
-//   const features_data_arr = Object.values(modal_data.features);
-//   // console.log(features_data_arr);
+    `);
+  // Modal features
+  const featuresListModal = document.getElementById('featuresListModal');
+  // console.log(modal_data.features);
+  const features_data_arr = Object.values(modal_data.features);
+  // console.log(features_data_arr);
 
-//   features_data_arr.forEach((ele) => {
-//     // console.log(ele.feature_name);
-//     // console.log(ele.feature_name.length);
-//     if (ele.feature_name) {
-//       const li = document.createElement('li');
-//       li.innerText = ele.feature_name;
-//       featuresListModal.appendChild(li);
-//     } else {
-//       const li = document.createElement('li');
-//       li.innerText = 'not found';
-//       featuresListModal.appendChild(li);
-//     }
-//   });
+  features_data_arr.forEach((ele) => {
+    // console.log(ele.feature_name);
+    // console.log(ele.feature_name.length);
+    if (ele.feature_name) {
+      const li = document.createElement('li');
+      li.innerText = ele.feature_name;
+      featuresListModal.appendChild(li);
+    } else {
+      const li = document.createElement('li');
+      li.innerText = 'not found';
+      featuresListModal.appendChild(li);
+    }
+  });
 
-//   // Modal integrations
-//   const modal_Integrations = document.getElementById('modal_Integrations');
+  // Modal integrations
+  const modal_Integrations = document.getElementById('modal_Integrations');
 
-//   if (modal_data.integrations === 0 || modal_data.integrations === null) {
-//     const li = document.createElement('li');
-//     li.innerText = 'not found';
-//     modal_Integrations.appendChild(li);
-//   } else {
-//     modal_data.integrations.forEach((ele) => {
-//       // console.log(ele);
-//       const li = document.createElement('li');
-//       li.innerText = ele;
-//       modal_Integrations.appendChild(li);
-//     });
-//   }
+  if (modal_data.integrations === 0 || modal_data.integrations === null) {
+    const li = document.createElement('li');
+    li.innerText = 'not found';
+    modal_Integrations.appendChild(li);
+  } else {
+    modal_data.integrations.forEach((ele) => {
+      // console.log(ele);
+      const li = document.createElement('li');
+      li.innerText = ele;
+      modal_Integrations.appendChild(li);
+    });
+  }
 
-//   // modal_accuracy
-//   // console.log(modal_data.pricing);
-//   // console.log(modal_data.pricing);
-//   const price_box = document.getElementById('price_box');
-//   if (modal_data.pricing === null || modal_data.pricing.length === 0) {
-//     const basic = document.createElement('div');
-//     basic.setAttribute('class', 'text-success bg-white fw-bold p-3 rounded');
-//     basic.innerHTML = ' Free of Cost';
-//     const pro = document.createElement('div');
-//     pro.setAttribute('class', 'text-success bg-white fw-bold p-3 rounded');
-//     pro.innerHTML = ' Free of Cost';
-//     const enterprise = document.createElement('div');
-//     enterprise.setAttribute(
-//       'class',
-//       'text-success bg-white fw-bold p-3 rounded'
-//     );
-//     enterprise.innerHTML = ' Free of Cost';
-//     price_box.appendChild(basic);
-//     price_box.appendChild(pro);
-//     price_box.appendChild(enterprise);
-//   } else {
-//     modal_data.pricing.forEach((ele) => {
-//       console.log(ele.plan);
-//       const basic = document.createElement('div');
-//       basic.setAttribute('class', 'text-success bg-white fw-bold p-3 rounded');
-//       basic.innerHTML = ` ${ele.price} ${ele.plan} `;
-//       price_box.appendChild(basic);
-//     });
-//   }
-// };
+  // modal_accuracy
+  const price_box = document.getElementById('price_box');
+  if (modal_data.pricing === null || modal_data.pricing.length === 0) {
+    const basic = document.createElement('div');
+    basic.setAttribute('class', 'text-success bg-white fw-bold p-3 rounded');
+    basic.innerHTML = ' Free of Cost';
+    const pro = document.createElement('div');
+    pro.setAttribute('class', 'text-success bg-white fw-bold p-3 rounded');
+    pro.innerHTML = ' Free of Cost';
+    const enterprise = document.createElement('div');
+    enterprise.setAttribute(
+      'class',
+      'text-success bg-white fw-bold p-3 rounded'
+    );
+    enterprise.innerHTML = ' Free of Cost';
+    price_box.appendChild(basic);
+    price_box.appendChild(pro);
+    price_box.appendChild(enterprise);
+  } else {
+    modal_data.pricing.forEach((ele) => {
+      console.log(ele.plan);
+      const basic = document.createElement('div');
+      basic.setAttribute('class', 'text-success bg-white fw-bold p-3 rounded');
+      basic.innerHTML = ` ${ele.price} ${ele.plan} `;
+      price_box.appendChild(basic);
+    });
+  }
+};
 
 // function spinner(isLoading) {
 //   if (isLoading) {
@@ -231,26 +229,3 @@ allData();
 //     document.getElementById('spinner').classList.add('d-none');
 //   }
 // }
-
-//  separate an array and an object from a mixed array
-
-// const mixedArray = [1, 'two', { name: 'John', age: 25 }, 'four', [5, 6]];
-// const arrayOnly = mixedArray.filter((item) => Array.isArray(item));
-// const objectOnly = mixedArray.filter(
-//   (item) => typeof item === 'object' && !Array.isArray(item)
-// );
-// console.log(arrayOnly); // [[5, 6]]
-// console.log(objectOnly); // [{ name: "John", age: 25 }]
-
-// const mixedArray = [1, "two", { name: "John", age: 25 }, "four", [5, 6]];
-// let arrayOnly = [];
-// let objectOnly = [];
-// for (let i = 0; i < mixedArray.length; i++) {
-//   if (Array.isArray(mixedArray[i])) {
-//     arrayOnly.push(mixedArray[i]);
-//   } else if (typeof mixedArray[i] === "object" && !Array.isArray(mixedArray[i])) {
-//     objectOnly.push(mixedArray[i]);
-//   }
-// }
-// console.log(arrayOnly); // [[5, 6]]
-// console.log(objectOnly); // [{ name: "John", age: 25 }]
